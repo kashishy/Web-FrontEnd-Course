2f7fb9f08f97edd01888349fb92bb4f580729eb2
Ashish Kumar <ashishkmrydv392@gmail.com>
2020-05-16 14:25:10 +0530

    NPM Scripts Part 1

---
+++ conFusion/js/scripts.js
@@ -0,0 +1,20 @@
+$("#carouselButton").click(function(){
+    if ($("#carouselButton").children("span").hasClass('fa-pause')) {
+        $("#mycarousel").carousel('pause');
+        $("#carouselButton").children("span").removeClass('fa-pause');
+        $("#carouselButton").children("span").addClass('fa-play');
+    }
+    else if ($("#carouselButton").children("span").hasClass('fa-play')){
+        $("#mycarousel").carousel('cycle');
+        $("#carouselButton").children("span").removeClass('fa-play');
+        $("#carouselButton").children("span").addClass('fa-pause');                    
+    }
+});
+
+$("#reserveTable").click(function(){
+    $('#reserveModal').modal('toggle');
+});
+
+$("#loginButton").click(function(){
+    $('#loginModal').modal('toggle');
+});
